1e9f46ca89aaf466981842fafdb89588799f12f3
anaghajayaraj <33338823+anaghajayaraj@users.noreply.github.com>
2020-09-13 12:55:27 +0530

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,33 @@
+function togglePrice() {
+    var x = document.getElementsByClassName('monthly-price')[0];
+    var y = document.getElementsByClassName('annual-price')[0];
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display="none";
+    }
+    else {
+      x.style.display = "none";
+      y.style.display="block";
+    }
+    var x = document.getElementsByClassName('monthly-price')[1];
+    var y = document.getElementsByClassName('annual-price')[1];
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display="none";
+    }
+    else {
+      x.style.display = "none";
+      y.style.display="block";
+    }
+    var x = document.getElementsByClassName('monthly-price')[2];
+    var y = document.getElementsByClassName('annual-price')[2];
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display="none";
+    }
+    else {
+      x.style.display = "none";
+      y.style.display="block";
+    }
+    
+  }
